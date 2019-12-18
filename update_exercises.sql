44c6f05364ce47335bec26c86968f3d528cddf87
Jacob Barrow <jacob.barrow@gmail.com>
2019-12-18 11:44:27 -0600

    completed update exercise

--- update_exercises.sql
+++ update_exercises.sql
@@ -1,16 +1,16 @@
 USE codeup_test_db;
-SELECT 'All albums' AS '';
+SELECT 'All albums with sales * 10' AS '';
 SELECT * FROM albums;
 UPDATE albums
 SET sales = (sales * 10);
 SELECT * FROM albums;
-SELECT 'Albums released before 1980' AS '';
+SELECT 'Albums released before 1980 changed to 1800s' AS '';
 SELECT * FROM albums WHERE release_date < 1980;
 UPDATE albums
 SET release_date = (release_date - 100)
 WHERE release_date < 1980;
 SELECT * FROM albums;
-SELECT 'ALL albums from Michael Jackson' AS '';
+SELECT 'ALL albums from Michael Jackson are changed to Peter Jackson' AS '';
 SELECT * FROM albums WHERE artist = 'Michael Jackson';
 UPDATE albums
 SET artist = 'Peter Jackson'
