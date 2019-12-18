3780456299866ec65afb04374ff030c57abc0d86
Jacob Barrow <jacob.barrow@gmail.com>
2019-12-18 11:24:35 -0600

    completed select exercises

---
+++ select_exercises.sql
@@ -0,0 +1,13 @@
+USE codeup_test_db;
+SELECT 'Pink Floyd' AS 'Here are the albums from:';
+SELECT name FROM albums WHERE artist = 'Pink Floyd';
+SELECT 'Pink Floyd' AS 'Here are the albums from:';
+SELECT release_date FROM albums WHERE name = 'Sgt. Pepper''s Lonely Hearts Club Band';
+SELECT 'MJ' AS 'Here are the albums from:';
+SELECT genre FROM albums WHERE name = 'Bad';
+SELECT 'the 1990s' AS 'Here are the albums from:';
+SELECT name FROM albums WHERE release_date BETWEEN 1990 AND 2000;
+SELECT 'Less than 20 million sales' AS 'Here are the albums that have:';
+SELECT name FROM albums WHERE sales < 20;
+SELECT 'Rock Albums' AS 'Albums with the genre of ''rock''';
+SELECT name FROM albums WHERE genre = 'Rock';
