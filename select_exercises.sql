5c5bff8dcdf16f866e02b6b7ad421c287dbcd680
Jacob Barrow <jacob.barrow@gmail.com>
2019-12-18 12:00:01 -0600

    updated beatles edit

--- select_exercises.sql
+++ select_exercises.sql
@@ -1,10 +1,13 @@
 USE codeup_test_db;
 SELECT 'Pink Floyd' AS 'Here are the albums from:';
 SELECT name FROM albums WHERE artist = 'Pink Floyd';
-SELECT 'Pink Floyd' AS 'Here are the albums from:';
+
+SELECT 'The Beatles' AS 'Here are the albums from:';
 SELECT release_date FROM albums WHERE name = 'Sgt. Pepper''s Lonely Hearts Club Band';
+
 SELECT 'MJ' AS 'Here are the albums from:';
 SELECT genre FROM albums WHERE name = 'Bad';
+
 SELECT 'the 1990s' AS 'Here are the albums from:';
 SELECT name FROM albums WHERE release_date BETWEEN 1990 AND 2000;
 SELECT 'Less than 20 million sales' AS 'Here are the albums that have:';
