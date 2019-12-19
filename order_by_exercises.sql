58d6436e9b4e1ed28c6edee770f91ab562e5f832
Jacob Barrow <jacob.barrow@gmail.com>
2019-12-18 18:44:06 -0600

    completed order by exercises

---
+++ order_by_exercises.sql
@@ -0,0 +1,28 @@
+USE employees;
+SELECT * FROM employees
+WHERE gender = 'M' AND (
+    first_name = 'Irena'
+    OR first_name = 'Maya'
+    OR first_name = 'Vidya')
+    ORDER BY last_name DESC, first_name DESC;
+SELECT * FROM employees
+    WHERE last_name LIKE 'E%'
+    OR last_name LIKE '%E';
+SELECT * FROM employees
+    WHERE last_name LIKE '%E'
+    AND last_name LIKE 'E%'
+    ORDER BY emp_no DESC;
+SELECT * FROM employees
+    WHERE hire_date BETWEEN '1990-01-01' AND '1999-12-31';
+SELECT * FROM employees
+    WHERE hire_date BETWEEN '1990-01-01' AND '1999-12-31'
+    AND birth_date LIKE '%-12-25'
+    ORDER BY birth_date DESC, hire_date;
+SELECT * FROM employees
+    WHERE birth_date LIKE '%-12-25';
+SELECT * FROM employees
+    WHERE last_name LIKE '%q%';
+SELECT * FROM employees
+    WHERE last_name LIKE '%q%'
+    AND last_name NOT LIKE '%qu%';
+
