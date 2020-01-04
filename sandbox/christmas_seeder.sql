694f20f271518270770b3e3b9aeaba1e9bb44bb8
Jacob Barrow <jacob.barrow@gmail.com>
2020-01-04 14:27:43 -0600

    adjusted quotation marks to single quotes

--- sandbox/christmas_seeder.sql
+++ sandbox/christmas_seeder.sql
@@ -1,5 +1,5 @@
 USE christmas_db;
 TRUNCATE people;
 INSERT INTO people (first_name, last_name, age, birthday, nice, wishlist)
-VALUES ("jacob", "barrow", 29, '1990-01-25', false, "sandwiches"),
-       ("jasmin", "eden", 32, '1987-09-05', true, "oinkers");
+VALUES ('jacob', 'barrow', 29, '1990-01-25', false, 'sandwiches'),
+       ('jasmin', 'eden', 32, '1987-09-05', true, 'oinkers');
